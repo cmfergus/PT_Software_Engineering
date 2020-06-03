e05849c8363fad208bc4e26667eefc6565890f76
PSCBUS_106 <pscbus_106@PSCBUS-106.local>
2020-06-03 18:25:44 -0400

    finished loop practice

---
+++ Mod1/week-1/w1d3/homework/loops.js
@@ -0,0 +1,48 @@
+// Create a loop that logs the numbers from 0-99 (ascending)let num = 0
+let num= 0
+while (num <= 99) {
+    console.log(num);
+    num++;
+}
+
+
+// Create a loop that logs the numbers from 99-0 (descending)
+
+
+let num= 99
+while (num >= 0) {
+    console.log(num);
+    num--;
+}
+
+
+// Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+
+
+
+let num = 0
+for (i= 3; i <= 90; i++) {
+    if (i % 3 == 0) {
+        console.log(i)
+    };
+   
+};
+
+
+// Create a loop that logs the numbers from 81-26 (descending)
+
+
+let num= 81
+while (num >= 26) {
+    console.log(num);
+    num--;
+};
+
+
+// Create a loop that logs the numbers from 49-72 (ascending)
+
+let num= 49
+while (num <= 70) {
+    console.log(num);
+    num++;
+};
