b133ce6475ad0e5e0a70411b2a3324d07e2cd3bf
PSCBUS_106 <pscbus_106@PSCBUS-106.local>
2020-06-09 09:17:31 -0400

    Wild Wild Life

--- Mod1/week-2/w2d2/homework/answers.js
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -29,18 +29,6 @@ if ((i % 2) == 0) {
 
 var i; 
 for (i=1; i <= 100; i++)
-if ((i % 3) == 0) {
-    console.log("Fizz");
-} else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("Fizzbuzz");
-} else if ((i % 5) == 0) {
-    console.log("Buzz");
-} else if (i % 3 == 0 && i % 5 == 0) {
-    console.log("Fizzbuzz");
-};
-
-var i; 
-for (i=1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
 } else if (i % 3 == 0) {
@@ -54,6 +42,24 @@ if (i % 3 == 0 && i % 5 == 0) {
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+const plantee = ["Plantee", "plant",  5001, "Mordor"]
+
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+const wolfy = ["Wolfy", "wolf", 16, "Gotham City"]
+
+// Give D'Art a second hometown by adding "Hawkins"
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down", "Hawkins"]
+
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+
+const Gameboy = ["Wolfy", "wolf", 16, "Yukon Territory"]
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
